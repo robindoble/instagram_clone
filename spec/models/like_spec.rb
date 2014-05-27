58317bbab85e81e8ff02ed5ccd464d551b768f5d
Robin <robindoble@gmail.com>
2014-05-27 20:33:33 +0100

    user model allows users to delete their own likes

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -8,25 +8,50 @@ describe Like do
 		@post2 = @user.posts.create(:caption => 'This is a post test')
 	end
 
-  it 'a user can only like a post once' do 
-  	like = @post1.likes.create
-  	like.user = @user
-  	expect(like.save).to eq(true)
-  	like = @post1.likes.create
-  	like.user = @user
-  	expect(like.save).to eq(false)
-  end
+	context 'adding like' do 
 
-  it 'user can like more than one post' do 
-  	like = @post1.likes.create
-  	like.user = @user
-  	expect(like.save).to eq(true)
-  	like = @post2.likes.create
-  	like.user = @user
-  	expect(like.save).to eq(true)
-  end
+	  it 'a user can only like a post once' do 
+	  	like = @post1.likes.create
+	  	like.user = @user
+	  	expect(like.save).to eq(true)
+	  	like = @post1.likes.create
+	  	like.user = @user
+	  	expect(like.save).to eq(false)
+	  end
+
+	  it 'user can like more than one post' do 
+	  	like = @post1.likes.create
+	  	like.user = @user
+	  	expect(like.save).to eq(true)
+	  	like = @post2.likes.create
+	  	like.user = @user
+	  	expect(like.save).to eq(true)
+	  end
+
+	end
 
 
+	context 'deleting likes' do 
+
+		it 'a user can unlike their like for any given post' do 
+			like = @post1.likes.create
+	  	like.user = @user
+	  	like.save
+	  	expect(Like.all.count).to eq(1)
+	  	like.destroy
+	  	expect(Like.all.count).to eq(0)
+		end
+
+		# it 'a user can unlike their like for any given post' do 
+		# 	like = @post1.likes.create
+	 #  	like.user = @user
+	 #  	like.save
+	 #  	expect(Like.all.count).to eq(1)
+	 #  	like.destroy
+	 #  	expect(Like.all.count).to eq(0)
+		# end
+
+	end
 
 
 end
