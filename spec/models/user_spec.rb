e46eff1baa5a2148d805ba5838d7284a8bae528a
Robin <robindoble@gmail.com>
2014-05-28 11:23:43 +0100

    deleting a user deletes their posts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe User do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+describe User do
+
+	before do 
+		@user = User.create(:username => 'robindoble', :email => 'robindoble@gmail.com',:password => 'password',:password_confirmation => 'password')
+		@post = @user.posts.create(:caption => 'This is a post test')
+	end
+
+	it 'deleting a user deletes all their posts' do 
+			# like = @post.likes.new
+	  	# like.user = @user
+	  	# like.save
+	  	@user.destroy 
+	  	expect(Post.all.count).to eq(0)
+	end
+ 
+
+
+end
+
