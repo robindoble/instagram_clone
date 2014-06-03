8574e465f425d391caae9b35cffe103c3bbb7b8a
Robin <robindoble@gmail.com>
2014-06-03 15:34:16 +0100

    orders display post id and username

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,7 @@ class OrdersController < ApplicationController
 	before_action :authenticate_admin!
 
 def index
-
+	@orders = Order.all
 end
 
 end
--- spec/features/order_feature_spec.rb
+++ spec/features/order_feature_spec.rb
@@ -4,7 +4,7 @@ describe 'Orders' do
 
 	before do 
 		@user = User.create(:username => 'hankmoody', :email => 'hank@california.com',:password => 'password',:password_confirmation => 'password')
-		@user.posts.create
+		@post = @user.posts.create
 		@admin = Admin.create
 	end
 
@@ -23,7 +23,38 @@ describe 'Orders' do
 			end
 		end
 
+		context 'with orders' do 
+			
 
+
+			it 'users should be directed back to signup' do 
+				visit '/orders'
+				expect(current_path).to eq('/admins/sign_in')
+			end
+		end
+
+
+		context 'with orders' do 
+
+			before do 
+				login_as @admin, scope: :admin
+				christmas_day = Date.new(2013, 12, 25)
+				Order.create(id: 1, post: @post, user: @user, created_at: christmas_day)
+				# puts Order.first.user.username
+				# puts Order.first.post.id
+				visit '/orders'
+			end
+
+			it 'should display the product' do 
+				expect(page).to have_link @post.id
+			end
+
+			it 'should display the product' do 
+				expect(page).to have_content @user.username
+			end
+
+
+		end
 
 
 
