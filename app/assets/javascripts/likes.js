318f2932509e676b82a33d9254d13d4b5163dd74
Robin <robindoble@gmail.com>
2014-06-04 10:58:12 +0100

    pushing to heroku

--- app/assets/javascripts/likes.js
+++ app/assets/javascripts/likes.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 			// var targetID = response.post_id
 			// var postID = $('.post-parent-container[data-id=' + targetID + ']');
 			
-			// var rendered = Mustache.render($('#new_like').html(),response);
+			var rendered = Mustache.render($('#new_like').html(),response);
 			// // console.log(postID)
 			// postID.find('ul[class="like-list"]').prepend(rendered);
 
@@ -22,9 +22,12 @@ $(document).ready(function(){
 		channel = connection.subscribe('likes');
 		channel.bind('new',function(like){
 			console.log(like)
+
 			// $('.like_list').append('<li> resp.new_like_count </li>');		
 			var postID = $('.post-parent-container[data-id=' + like.id + ']');
-			postID.find('ul[class="like-list"]').append('<li>' + like.new_like_user + ' likes' + ' </li>' );
+			var rendered = Mustache.render($('#new_like').html(),like);
+			postID.find('ul[class="like-list"]').prepend(rendered);
+			// postID.find('ul[class="like-list"]').append('<li>' + like.new_like_user + ' likes' + ' </li>' );
 			// append('<li>'+ like.user +'</li>');
 			// console.log('a new like registered');
 		});
