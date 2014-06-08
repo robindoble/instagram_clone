761a6496cfa6403d1ce8fe1811bc8b44b32fd456
Robin <robindoble@gmail.com>
2014-06-08 14:35:30 +0100

    relationship model responds to follower and followed

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -8,14 +8,17 @@ describe Relationship do
 	before do 
 		@user1 = User.create(:username => 'robindoble', :email => 'robindoble@gmail.com',:password => 'password',:password_confirmation => 'password')
 		@user2 = User.create(:username => 'hankmoody', :email => 'hank@california.com',:password => 'password',:password_confirmation => 'password')	
+		@relationship = @user1.relationships.new(followed_id: @user2)
 	end
 
   it 'should be able to save a relationship' do 
-  	relationship = @user1.relationships.new(followed_id: @user2)
-  	relationship.save!
+  	@relationship.save!
 	end
 
-
+  it 'should respond to following' do 
+  	@relationship.should respond_to(:follower)
+  	@relationship.should respond_to(:followed)
+	end
 
 
 end
